ea725d9bdb75a36b4c1af42a9fa9ef7ce1e225ab
action <action@github.com>
2025-07-15 12:19:44 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,13 +1,17 @@
-//Tue Jul 15 2025 12:18:34 GMT+0000 (Coordinated Universal Time)
+//Tue Jul 15 2025 12:19:44 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-function _0x1abc() {
-  var _0x5f2b9c = new Date().getTime();
-  return Math.floor(_0x5f2b9c / 1000);
-}
-function _0xa3fd() {
-  var _0x11d2c9 = _0x1abc();
-  var _0x3c49df = shareUrl + _0x11d2c9 + "yongzhi";
-  var _0x4a31b6 = md5(_0x3c49df);
-  return _0x4a31b6;
-}
+(function () {
+  var _0x2e1f = function (i) {
+    return i.toString(36);
+  };
+  var _0xabc = function () {
+    return Math['floor'](Date['now']() / 0x3e8);
+  };
+  var _0xdef = function () {
+    var _0x1 = _0xabc();
+    var _0x2 = window['\x73\x68\x61\x72\x65\x55\x72\x6C'] + _0x1 + '\x79\x6F\x6E\x67\x7A\x68\x69';
+    return window['\x6D\x64\x35'](_0x2);
+  };
+  window[_0x2e1f(100)] = _0xdef;
+})();
